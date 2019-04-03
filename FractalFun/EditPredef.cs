1f1d9263a01ed490d6da61a7e94b48d17a32e01c
Dan Rhea <danrhea@bellsouth.net>
2019-04-03 12:17:40 -0400

    Fleshed out the comments

--- FractalFun/EditPredef.cs
+++ FractalFun/EditPredef.cs
@@ -15,12 +15,15 @@ using System.IO;
 namespace FractalFun
 {
     /// <summary>
-    /// 
+    /// A specialized simple editor for editing the json file PredefinedAttractors.json.
+    /// The editor should be reusabe as it loads a hard coded file, allows editing and 
+    /// the ability to save the file. When the editor is exited (runs as a modal dialog)
+    /// the file is reprocessed and used to load the dropdown in the Attractory form.
     /// </summary>
     public partial class EditPredef : Form
     {
         /// <summary>
-        /// 
+        /// Initialize the form... nothing to see here... move along
         /// </summary>
         public EditPredef()
         {
@@ -28,13 +31,15 @@ namespace FractalFun
         }
 
         /// <summary>
-        /// 
+        /// On load, open and load the PredefinedAttractors file
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">EditPrefs form</param>
+        /// <param name="e">any parameters</param>
         private void EditPredef_Load(object sender, EventArgs e)
         {
             TxtEdit.Clear();
+            // At some point in the future, this will get the filename from the 
+            // parent form since hard coding is almost evil :)
             using (StreamReader r = new StreamReader("PredefinedAttractors.json"))
             {
                 string json = r.ReadToEnd();
@@ -45,10 +50,11 @@ namespace FractalFun
         }
 
         /// <summary>
-        /// 
+        /// Save whatever edits we may have been made... again with the hardcoded 
+        /// filename
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">Save button</param>
+        /// <param name="e">Params if any</param>
         private void ToolStripButton1_Click(object sender, EventArgs e)
         {
             string json = TxtEdit.Text;
@@ -58,7 +64,6 @@ namespace FractalFun
                 j.Flush();
                 j.Close();
             }
-           
         }
     }
 }
